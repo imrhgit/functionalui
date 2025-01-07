b0805d02345ea35d66333b0c4a83b3838448953e
IH <ih@IH.local>
2025-01-07 12:27:38 +0800

    update configs to reflect library mode -- follow npm_packages/localfunctional/functionalui. add lib main.ts and types.ts, only copy css files. update package.json. bump version to 0.0.5 -- previously on localpackages/functionalui on 0.0.4052.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import preserveDirectives from "rollup-preserve-directives";
+import { defineConfig, type Plugin } from "vite";
+import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		dts({ rollupTypes: true }),
+		preserveDirectives() as Plugin,
+	],
+	build: {
+		// library entry and output settings
+		lib: {
+			entry: {
+				main: resolve(__dirname, "lib/main.ts"),
+				types: resolve(__dirname, "lib/styles/types/types.ts"),
+			},
+		},
+		// bundler options
+		// externalize react-related imports
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+					"react/jsx-runtime": "react/jsx-runtime",
+				},
+			},
+		},
+	},
+});
