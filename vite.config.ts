f5581ea6f5f49b73f3e09c0991676922e7461e46
IH <ih@IH.local>
2025-01-20 17:09:57 +0800

    .gitignore changes. /hk UIcon. package.json -- bump version to 0.0.5002; dist/style.css to dist/functionalui.css. vite.config.ts -- dts add tsconfigPath; from docs, had issue with exporting types.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	plugins: [
 		react(),
 		tsconfigPaths(),
-		dts({ rollupTypes: true }),
+		dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
 		preserveDirectives() as Plugin,
 	],
 	build: {
